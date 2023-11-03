1a5e7021f868c8fa7a4fa796f17c8bcb0f78aad5
Ag-crane <enaklee401@dongguk.edu>
2023-11-04 04:50:47 +0900

    Feat: 예약현황 데이터 Datetime 형식으로 형식으로 크롤링, DB에 저장

--- crawler/reservation-crawler.js
+++ crawler/reservation-crawler.js
@@ -25,14 +25,16 @@ async function getAvailableTime(prId, roomId, date) {
 
   await page.goto(url, {
     waitUntil: "networkidle0",
+    timeout: 60000,
   });
 
   await page.waitForSelector('a[class="calendar-date"] > span[class="num"]');
-  console.log("로드 완료");
-  page.on("console", (msg) => console.log(msg.text())); // 디버깅용
 
-  // 현재 날짜 확인
-  await getStartDate(page);
+  // 디버깅용 - puppeteer 브라우저 안에서 console.log 실행
+  page.on("console", (msg) => console.log(msg.text()));
+
+  // // 현재 날짜 확인
+  // await getStartDate(page);
 
   // 타겟 날짜가 달력에 있는지 먼저 확인
   let targetDateElement = await page.$$eval(
@@ -45,10 +47,10 @@ async function getAvailableTime(prId, roomId, date) {
   );
 
   if (targetDateElement) {
-    console.log("Element found");
+    // console.log("Element found");
   } else {
-    console.log("Element not found");
-    console.log("달력 넘기기");
+    // console.log("Element not found");
+    // console.log("달력 넘기기");
     await page.evaluate(() => {
       const nextMonthButton = document.querySelector('a[title="다음 달"]');
       nextMonthButton.click();
@@ -56,7 +58,7 @@ async function getAvailableTime(prId, roomId, date) {
     await page.waitForSelector(
       'td:not(.calendar-unselectable) > a[class="calendar-date"] > span[class="num"]'
     );
-    console.log("달력 넘기기 성공");
+    // console.log("달력 넘기기 성공");
   }
 
   // 타겟 날짜 재설정
@@ -69,7 +71,7 @@ async function getAvailableTime(prId, roomId, date) {
     date
   );
 
-  console.log(targetDateElement);
+  // console.log(targetDateElement);
   await page.$$eval(
     'td:not(.calendar-unselectable) > a[class="calendar-date"] > span[class="num"]',
     (elements, date) => {
@@ -78,9 +80,15 @@ async function getAvailableTime(prId, roomId, date) {
     },
     date
   );
-  await page.waitForTimeout(100)
-  // 선택 후 현재 날짜 확인
-  await getStartDate(page);
+  await page.waitForTimeout(100);
+  // // 선택 후 현재 날짜 확인
+  // await getStartDate(page);
+
+  // 선택한 날짜 저장
+  const selectedDate = await page.$eval(
+    'td[class*="calendar-selected"][class*="start-day"][data-cal-datetext]',
+    (element) => element.getAttribute("data-cal-datetext")
+  );
 
   // 예약가능시간 가져오기
   const availableTimes = await page.$$eval(
@@ -88,10 +96,23 @@ async function getAvailableTime(prId, roomId, date) {
     (elements) => elements.map((el) => el.textContent)
   );
 
+  // 선택한 날짜와 예약가능시간을 결합하여 datetime 변수 생성
+  const datetimeValues = availableTimes.map(
+    (time) => `${selectedDate} ${time}`
+  );
+
   await browser.close();
 
-  return availableTimes;
+  return datetimeValues;
 }
+
+// async function main() {
+//   const result = await getAvailableTime(329314, 3355287, 5);
+//   console.log(result);
+// }
+// main();
+
+async function getDataAndInsert() {
   const connection = await mysql.createConnection(dbConfig);
 
   await connection.execute(`
@@ -100,3 +121,35 @@ async function getAvailableTime(prId, roomId, date) {
     available_time DATETIME NOT NULL,
     PRIMARY KEY (room_id, available_time)
   )`);
+
+  try {
+    const [rows] = await connection.execute(
+      "SELECT pr_id, room_id FROM room_datas"
+    );
+    for (const row of rows) {
+      //룸별
+      for (let date = 1; date <= 30; date++) {
+        //날짜별  // 임시로 1~30만 처리
+        const availableTimes = await getAvailableTime(
+          row.pr_id,
+          row.room_id,
+          date
+        );
+
+        for (const time of availableTimes) {
+          await connection.execute(
+            `INSERT INTO reservation_datas (room_id, available_time) VALUES (?, ?) ON DUPLICATE KEY UPDATE available_time = ?`,
+            [row.room_id, time, time]
+          );
+        }
+        console.log("room : ", row.room_id, "날짜 : ", date);
+      }
+    }
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    await connection.end();
+  }
+}
+
+getDataAndInsert();
