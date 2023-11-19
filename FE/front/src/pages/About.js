5ce9b90548153f64c16971d7a8ce6a70afe075e3
star2t <star2thb@gmail.com>
2023-11-19 13:38:21 +0900

    Feat: 전체목록 페이지 API 적용

--- FE/front/src/pages/About.js
+++ FE/front/src/pages/About.js
@@ -3,8 +3,8 @@ import ListCard from "../components/Card/ListCard";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Filter from "../components/Dropdown/Filter";
-import "../styles/pages/About.css";
 import Pagination from "@mui/material/Pagination";
+import "../styles/pages/About.css";
 
 function About() {
   const [cardData, setCardData] = useState([]);
@@ -13,79 +13,167 @@ function About() {
   const itemsPerPage = 8;
   const [filteredCardData, setFilteredCardData] = useState([]);
   const [uniqueCommonAddresses, setUniqueCommonAddresses] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [filteredTotalPages, setFilteredTotalPages] = useState(1);
 
   useEffect(() => {
-    async function fetchCardData() {
+    async function fetchAllCardData() {
       try {
-        const response = await fetch("http://43.200.181.187:8080/practice-rooms/sorted-by-name?page=0&size=8");
+        const response = await fetch(
+          `http://43.200.181.187:8080/practice-rooms/sorted-by-name?page=0&size=${itemsPerPage}`
+        );
+
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
-        const data = await response.json();
-        
+
+        const responseData = await response.json();
+        const totalElements = responseData.totalElements;
+
+        let allCardData = responseData.content;
+
+        for (let page = 1; page < responseData.totalPages; page++) {
+          const nextPageResponse = await fetch(
+            `http://43.200.181.187:8080/practice-rooms/sorted-by-name?page=${page}&size=${itemsPerPage}`
+          );
+
+          if (!nextPageResponse.ok) {
+            throw new Error("Failed to fetch data for page " + page);
+          }
+
+          const nextPageData = await nextPageResponse.json();
+          allCardData = [...allCardData, ...nextPageData.content];
+        }
+
         const uniqueAddresses = [
-          ...new Set(data.map((card) => card.commonAddress.trim())),
-        ];
-        uniqueAddresses.sort((a, b) => a.localeCompare(b));
+          ...new Set(allCardData.map((card) => card.commonAddress.trim())),
+        ].sort((a, b) => a.localeCompare(b));
 
         setUniqueCommonAddresses(uniqueAddresses);
-        setCardData(data);
-        setFilteredCardData(data);
+        setCardData(allCardData);
+        setFilteredCardData(allCardData);
+        setTotalPages(Math.ceil(totalElements / itemsPerPage));
+        setFilteredTotalPages(Math.ceil(totalElements / itemsPerPage));
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     }
 
-    fetchCardData();
-  }, []);
+    fetchAllCardData();
+  }, [itemsPerPage]);
 
   const renderFilteredCards = () => {
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-
-    return filteredCardData
-      .slice(startIndex, endIndex)
-      .map((card, index) => (
-        <ListCard
-          key={index}
-          title={card.name}
-          cost={card.cost}
-          locate={card.fullAddress}
-          content={<img src={card.imageUrl} alt="사진이 없습니다." />}
-        />
-      ));
+  
+    const cardsToRender = selectedFilter
+      ? filteredCardData.slice(startIndex, endIndex)
+      : cardData.slice(startIndex, endIndex);
+  
+    return cardsToRender.map((card, index) => (
+      <ListCard
+        key={index}
+        title={card.name}
+        cost={card.cost}
+        locate={card.fullAddress}
+        content={<img src={card.imageUrl} alt="사진이 없습니다." />}
+      />
+    ));
   };
 
-  const handleFilterChange = (selectedValue) => {
+  const handleFilterChange = async (selectedValue) => {
     setSelectedFilter(selectedValue);
-
-    const filteredData = cardData.filter(
-      (card) => card.commonAddress === selectedValue
-    );
-
-    setFilteredCardData(filteredData);
     setCurrentPage(1);
+  
+    try {
+      const totalResponse = await fetch(
+        `http://43.200.181.187:8080/practice-rooms/sorted-by-name?page=0&size=${itemsPerPage}`
+      );
+  
+      if (!totalResponse.ok) {
+        throw new Error("Failed to fetch total data");
+      }
+  
+      const totalData = await totalResponse.json();
+      const totalElements = totalData.totalElements;
+  
+      let filteredCardData = [];
+  
+      for (let page = 0; page < totalData.totalPages; page++) {
+        const response = await fetch(
+          `http://43.200.181.187:8080/practice-rooms/sorted-by-name?page=${page}&size=${itemsPerPage}`
+        );
+  
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data for page ${page}`);
+        }
+  
+        const responseData = await response.json();
+  
+        const filteredPageData = responseData.content.filter(
+          (card) =>
+            !selectedValue ||
+            card.commonAddress.trim() === selectedValue.trim()
+        );
+  
+        filteredCardData = [...filteredCardData, ...filteredPageData];
+      }
+  
+      const filteredTotalPages = Math.ceil(
+        filteredCardData.length / itemsPerPage
+      );
+  
+      setFilteredCardData(filteredCardData);
+      setFilteredTotalPages(filteredTotalPages || 1);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
-
-  const handlePageChange = (event, newPage) => {
+  
+  const handlePageChange = async (event, newPage) => {
     setCurrentPage(newPage);
-  };
+  
+    try {
+      const response = await fetch(
+        `http://43.200.181.187:8080/practice-rooms/sorted-by-name?page=${newPage - 1}&size=${itemsPerPage}`
+      );
+  
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data for page ${newPage}`);
+      }
+  
+      const responseData = await response.json();
+  
+      const filteredPageData = selectedFilter
+        ? responseData.content.filter(
+            (card) => card.commonAddress.trim() === selectedFilter.trim()
+          )
+        : responseData.content;
 
-  const totalPages = Math.ceil(filteredCardData.length / itemsPerPage);
+      const startIndex = (newPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      
+      const cardsForCurrentPage = filteredPageData.slice(startIndex, endIndex);
+  
+      setFilteredCardData(cardsForCurrentPage);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   return (
     <div>
       <Header />
       <div className="filter-container">
         <Filter
-          options={uniqueCommonAddresses}
+          allUniqueAddresses={uniqueCommonAddresses}
           onChange={handleFilterChange}
         />
       </div>
       <div className="card_pack init_height">{renderFilteredCards()}</div>
       <div className="pagination">
         <Pagination
-          count={totalPages}
+          count={selectedFilter ? filteredTotalPages : totalPages}
           page={currentPage}
           onChange={handlePageChange}
         />
