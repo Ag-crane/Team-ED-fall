d081e120ef1a701e98260825bf73c8a96b38f775
star2t <star2thb@gmail.com>
2023-11-20 00:25:30 +0900

    Feat: 통합 검색 API 연동

--- FE/front/src/pages/Home.js
+++ FE/front/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import MainCard from "../components/Card/MainCard";
@@ -11,51 +11,102 @@ import "../styles/pages/Home.css";
 function Home() {
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedTimes, setSelectedTimes] = useState([]);
-
-  const [cards, setCards] = useState([
-    {
-      id: 1,
-      title: "합주실 1",
-      cost: "가격 1",
-      locate: "위치 1",
-      content: "카드 내용 1",
-      isFavorite: false,
-    },
-    {
-      id: 2,
-      title: "합주실 2",
-      cost: "가격 2",
-      locate: "위치 2",
-      content: "카드 내용 2",
-      isFavorite: true,
-    },
-    {
-      id: 3,
-      title: "합주실 3",
-      cost: "가격 3",
-      locate: "위치 3",
-      content: "카드 내용 3",
-      isFavorite: false,
-    },
-    {
-      id: 4,
-      title: "합주실 4",
-      cost: "가격 4",
-      locate: "위치 4",
-      content: "카드 내용 4",
-      isFavorite: false,
-    },
-    {
-      id: 5,
-      title: "합주실 5",
-      cost: "가격 5",
-      locate: "위치 5",
-      content: "카드 내용 5",
-      isFavorite: false,
-    },
-  ]);
-
+  const [cards, setCards] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [groupedCards, setGroupedCards] = useState({});
+
+  useEffect(() => {
+    console.log("Inside useEffect");
+
+    async function fetchData() {
+      try {
+        console.log("Inside fetchData");
+
+        const formatDate = (date) => {
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, "0");
+          const day = String(date.getDate()).padStart(2, "0");
+          return `${year}-${month}-${day}`;
+        };
+
+        const formatTime = (timeString, addHour = 0) => {
+          const [hour, minute, meridian] = timeString.split(/:|\s/);
+          let hours = parseInt(hour, 10);
+
+          if (addHour) {
+            hours += 1;
+          }
+
+          if (meridian === "PM" && hours < 12) {
+            hours += 12;
+          } else if (meridian === "AM" && hours === 12) {
+            hours = 0;
+          }
+
+          return `${String(hours).padStart(2, "0")}:${minute}:00`;
+        };
+
+        console.log("Selected Date Type:", selectedDate);
+        console.log(
+          "Selected Times Type:",
+          typeof selectedTimes,
+          selectedTimes
+        );
+
+        const isValidDate = selectedDate instanceof Date;
+        const isValidTimes =
+          selectedTimes.length > 0 &&
+          selectedTimes.every((time) => {
+            const date = new Date(`2000-01-01 ${time}`);
+            return !isNaN(date.getTime());
+          });
+
+        console.log("isValidDate:", isValidDate);
+        console.log("isValidTimes:", isValidTimes);
+
+        if (isValidDate && isValidTimes) {
+          const dateParam = formatDate(selectedDate);
+          const startTimeParam = formatTime(selectedTimes[0]);
+          const endTimeParam = formatTime(
+            selectedTimes[selectedTimes.length - 1],
+            1
+          );
+
+          const response = await fetch(
+            `http://43.200.181.187:8080/rooms/available?date=${dateParam}&startTime=${startTimeParam}&endTime=${endTimeParam}`
+          );
+
+          if (!response.ok) {
+            const errorMessage = `Failed to fetch data. Status: ${response.status} ${response.statusText}`;
+            throw new Error(errorMessage);
+          }
+
+          const data = await response.json();
+
+          // Group roomNames by practiceRoomName
+          const groupedCards = data.reduce((acc, card) => {
+            const { practiceRoomName, roomName } = card;
+            if (!acc[practiceRoomName]) {
+              acc[practiceRoomName] = [];
+            }
+            acc[practiceRoomName].push(roomName);
+            return acc;
+          }, {});
+
+          setGroupedCards(groupedCards);
+          setCards(data);
+        } else {
+          console.error("Invalid date or times");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error.message);
+      }
+    }
+
+    if (selectedDate && selectedTimes.length > 0) {
+      fetchData();
+    }
+  }, [selectedDate, selectedTimes]);
 
   const nextCard = () => {
     const nextIndex = (currentIndex + 2) % cards.length;
@@ -70,22 +121,13 @@ function Home() {
   const currentCard1 = cards[currentIndex];
   const currentCard2 = cards[(currentIndex + 1) % cards.length];
 
-  const toggleFavorite = (id) => {
-    const updatedCards = cards.map((card) =>
-      card.id === id ? { ...card, isFavorite: !card.isFavorite } : card
-    );
-    setCards(updatedCards);
-  };
-
-  const selectedDateTime =
-    selectedDate && selectedTimes.length > 0
-      ? `${selectedDate.toLocaleDateString()} ${selectedTimes.join(", ")}`
-      : null;
+  console.log("currentCard1:", currentCard1);
+  console.log("currentCard2:", currentCard2);
 
   return (
     <div>
       <Header />
-      <div className={`content ${selectedDateTime ? "content-shifted" : ""}`}>
+      <div className={`content ${cards.length > 0 ? "content-shifted" : ""}`}>
         <p className="time_title">원하는 시간을 선택하세요</p>
         <div className="selector">
           <DateSelector
@@ -93,35 +135,31 @@ function Home() {
             setSelectedDate={setSelectedDate}
           />
           <p />
-          <TimeSelector selectedTimes={selectedTimes} setSelectedTimes={setSelectedTimes} />
-          {selectedDateTime && (
-            <p className="datetime">선택한 날짜와 시간: {selectedDateTime}</p>
+          <TimeSelector
+            selectedTimes={selectedTimes}
+            setSelectedTimes={setSelectedTimes}
+          />
+        </div>
+        <div>
+          {cards.length > 0 && (
+            <div className="maincard_box">
+              <div className="nav_buttons">
+                <button onClick={prevCard}>
+                  <FontAwesomeIcon icon={faArrowLeft} />
+                </button>
+              </div>
+              <div className="maincard_group">
+                <MainCard card={currentCard1} />
+                <MainCard card={currentCard2} />
+              </div>
+              <div className="nav_buttons">
+                <button onClick={nextCard}>
+                  <FontAwesomeIcon icon={faArrowRight} />
+                </button>
+              </div>
+            </div>
           )}
         </div>
-        {selectedDateTime && (
-          <div className="maincard_box">
-            <div className="nav_buttons">
-              <button onClick={prevCard}>
-                <FontAwesomeIcon icon={faArrowLeft} />
-              </button>
-            </div>
-            <div className="maincard_group">
-              <MainCard
-                card={currentCard1}
-                toggleFavorite={() => toggleFavorite(currentCard1.id)}
-              />
-              <MainCard
-                card={currentCard2}
-                toggleFavorite={() => toggleFavorite(currentCard2.id)}
-              />
-            </div>
-            <div className="nav_buttons">
-              <button onClick={nextCard}>
-                <FontAwesomeIcon icon={faArrowRight} />
-              </button>
-            </div>
-          </div>
-        )}
       </div>
       <Footer />
     </div>
