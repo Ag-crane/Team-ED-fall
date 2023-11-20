f5d70df3e003a3562903b522011d7d860f1c80d9
Ag-crane <enaklee401@dongguk.edu>
2023-11-20 16:42:36 +0900

    Feat: 크롤링 결과를 json 파일로 저장

--- crawler/realtime-crawler-parent.js
+++ crawler/realtime-crawler-parent.js
@@ -1,16 +1,35 @@
 import { splitArrayIntoChunks, getRoomId } from "./module.js";
 import { fork } from "child_process";
+import { writeFile } from 'fs/promises';
+
+async function saveResultsToFile(data, filename) {
+    const jsonData = JSON.stringify(data, null, 4); // 예쁘게 출력하기 위해 indent 사용
+    await writeFile(filename, jsonData, 'utf8');
+    console.log(`Results saved to ${filename}`);
+}
 
 const roomIds = await getRoomId();
 const chunks = splitArrayIntoChunks(roomIds, 5);
 // const region = process.argv[2]
 const date = process.argv[2];
-// console.log(chunks)
+
+const results = []; // 결과를 저장할 배열
 
 chunks.forEach(chunk => {
-	const child = fork('realtime-crawler.js');
+    const child = fork('realtime-crawler.js', { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
 	child.send({ date, chunk });
-	child.on('message', (result) => {
+
+	child.on('message', async(result) => {
+		results.push(result);
+
+        if (results.length === chunks.length) {
+            try {
+                await saveResultsToFile(results, 'results.json');
+            } catch (err) {
+                console.error('Error saving results to file:', err);
+            }
+        }
+
 		if ('duration' in result) {
 				console.log(`Total duration for this chunk: ${result.duration} milliseconds`);
 		} else if (result.success) {
