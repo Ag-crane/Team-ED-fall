368ff7fdb3272f01da4ab16b698ef57700ed4da0
star2t <star2thb@gmail.com>
2023-12-01 14:11:42 +0900

    Feat: 지도 현재 위치 중심 및 마커 클릭시 모달 open

--- FE/front/src/pages/Rating.js
+++ FE/front/src/pages/Rating.js
@@ -1,23 +1,40 @@
 import React, { useState, useEffect } from "react";
-import ListCard from "../components/Card/ListCard";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import RatingCard from "../components/Card/RatingCard";
-import "../styles/pages/Rating.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import RatingCard from "../components/Card/RatingCard";
+import Modal from "../components/Modal";
 import markerImg from "../assets/marker.png";
+import "../styles/pages/Rating.css";
+import "../styles/components/Modal.css"
 
 function Rating() {
   const [cardData, setCardData] = useState([]);
   const [locationData, setLocationData] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [hoveredLocation, setHoveredLocation] = useState(null);
+  const [userLocation, setUserLocation] = useState(null);
+  const {
+    id,
+    name,
+    commonAddress,
+    fullAddress,
+    phone,
+    virtualPhone,
+    imageUrl,
+    hasBooking,
+    bookingUrl,
+    visitorReviewScore,
+  } = cardData;
 
   useEffect(() => {
     fetchHighestRatedPracticeRooms();
     fetchAllPracticeLocations();
+    getUserLocation();
   }, []);
 
   const fetchHighestRatedPracticeRooms = async () => {
@@ -50,6 +67,35 @@ function Rating() {
     }
   };
 
+  const getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({ lat: latitude, lng: longitude });
+        //console.log("User location set:", { lat: latitude, lng: longitude });
+      },
+      (error) => {
+        console.error("Error getting user location:", error);
+      }
+    );
+  };
+
+  const handleMarkerHover = (location) => {
+    setHoveredLocation(location);
+  };
+
+  const handleMarkerHoverOut = () => {
+    setHoveredLocation(null);
+  };
+
+  const handleMarkerClick = (location) => {
+    setSelectedLocation(location);
+  };
+
+  const handleModalClose = () => {
+    setSelectedLocation(null);
+  };
+
   const renderMapMarkers = () => {
     return locationData.map((location, index) => (
       <Marker
@@ -59,8 +105,24 @@ function Rating() {
           iconUrl: markerImg,
           iconSize: [25, 30],
         })}
+        eventHandlers={{
+          click: () => handleMarkerClick(location),
+          mouseover: () => handleMarkerHover(location),
+          mouseout: () => handleMarkerHoverOut(),
+        }}
       >
-        <Popup>{location.name}</Popup>
+        <Tooltip
+          className="custom_tooltip"
+          permanent={true}
+          direction="top"
+          offset={[0, -10]}
+        >
+          {hoveredLocation === location && (
+            <div>
+              <h5>{location.name}</h5>
+            </div>
+          )}
+        </Tooltip>
       </Marker>
     ));
   };
@@ -78,18 +140,26 @@ function Rating() {
     ));
   };
 
+  const mapCenter = userLocation ? [userLocation.lat, userLocation.lng] : null;
+  //console.log('User location:', userLocation);
+
+  useEffect(() => {
+    //console.log('Map Center Updated:', mapCenter);
+  }, [mapCenter]);
 
   return (
     <div>
       <Header />
-      <MapContainer
-        center={[37.5562, 126.9239]}
-        zoom={16}
-        style={{ height: "400px", paddingTop: "10px", zIndex: "1" }}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        {renderMapMarkers()}
-      </MapContainer>
+      {mapCenter && (
+        <MapContainer
+          center={mapCenter}
+          zoom={16}
+          style={{ height: "400px", paddingTop: "10px", zIndex: "1" }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          {renderMapMarkers()}
+        </MapContainer>
+      )}
       <div>
         <div className="rating_title">
           평점 높은 순
@@ -102,6 +172,35 @@ function Rating() {
         </div>
         <div className="rating_card_pack">{renderCards()}</div>
       </div>
+      <Modal isOpen={!!selectedLocation} onClose={handleModalClose}>
+        {selectedLocation && (
+          <div className="modal-content">
+          <h2 className="modal-title">{selectedLocation.name}</h2>
+          <div className="modal-img-box">
+            <img className="modal-image" src={selectedLocation.imageUrl} alt={name} />
+          </div>
+          
+          <div className="modal-details">
+            <p>
+              <strong>주소:</strong> {selectedLocation.fullAddress}
+            </p>
+            <p>
+              <strong>연락처:</strong> {selectedLocation.phone || selectedLocation.virtualPhone}
+            </p>
+            <p>
+              <strong>방문자 평점:</strong>{" "}
+              {selectedLocation.visitorReviewScore ? selectedLocation.visitorReviewScore : "-"}
+            </p>
+            <button
+              onClick={() => window.open(selectedLocation.bookingUrl, "_blank")}
+              disabled={hasBooking !== "True"}
+            >
+              예약 페이지로 이동
+            </button>
+          </div>
+        </div>
+        )}
+      </Modal>
       <Footer />
     </div>
   );
