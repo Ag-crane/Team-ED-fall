c1c88b9fec3337bebec29b4ce9a739a6cbbdafff
star2t <star2thb@gmail.com>
2023-12-04 18:09:09 +0900

    Feat: 소셜 로그인 기능 추가

---
+++ FE/front/src/components/Login/UserInfo.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import profileImg from "../../assets/profile.png";
+import LoginBtn from "./LoginBtn";
+import "../../styles/components/Sidebar.css";
+
+function UserInfo() {
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    // 토큰을 가져오기
+    const token = localStorage.getItem("authToken");
+
+    // 백엔드 서버에 사용자 정보 요청
+    fetch("http://43.200.181.187:8080/user/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // 사용자 정보 업데이트
+        setUserInfo(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching user info:", error);
+      });
+  }, [navigate]);
+
+  if (!userInfo) {
+    return(
+    <>
+      <img className="profile_picture" src={profileImg} alt="프로필" />
+      <div className="login_text">
+        <p className="nickname">게스트</p>
+        <LoginBtn />
+      </div>
+    </>
+    );
+  }
+
+  return (
+    <>
+      <img className="profile_picture" src={profileImg} alt="프로필" />
+      <div className="login_text">
+        <p className="nickname">{userInfo.name} 님</p>
+      </div>
+    </>
+  );
+}
+
+export default UserInfo;
